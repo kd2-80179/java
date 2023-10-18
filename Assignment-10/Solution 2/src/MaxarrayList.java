2e4ba3e93e08af601be8eacec8d268ffcc5f93d7
kd2-80179 <sonamchourasiya721@gmail.com>
2023-10-18 21:02:35 +0530

    assignment 10 uploaded

---
+++ Assignment-10/Solution 2/src/MaxarrayList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+ 
+ abstract class MaxarrayList_Comparator implements Comparator<MaxarrayList_Comparator> {
+		@Override
+		public int compare(MaxarrayList e1, Student e2) {
+			int diff = e2.getName().compareTo(e1.getName());
+			if (diff == 0)
+				
+		list1.add("sonam");
+		list1.add("pgdac");
+		list1.add("dac");
+		list1.add("ditis");
+		list1.add("dbda");
+		
+		for (Integer ele : list1) {
+			System.out.println(ele);
+		}
+		Collections.length(list);
+		System.out.println("length List: " + list);
+		
+		
+		
+		
+
+	
+	
+
+}
+
+	private void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+}
