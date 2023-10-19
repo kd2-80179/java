c433ef21fb7c2360bea61365a5242b69c36af70d
kd2-80179 <sonamchourasiya721@gmail.com>
2023-10-19 21:27:37 +0530

    assignment -8,9,10,11 uploaded

--- Assignment11/Solution6/src/com/sunbeam/Mainsol6.java
+++ Assignment11/Solution6/src/com/sunbeam/Mainsol6.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 
 public class Mainsol6 {
 	static int menu() {
-		int choice;
+		int choice = 0;;
+		Scanner sc =new Scanner(System.in);
 		System.out.println("------------------------------------------------------");
 		System.out.println("Menu");
 		System.out.println("1.Accept");
 		System.out.println("2.find in map ");
-		
-		
+		System.out.println("enter choice");
+		choice=sc.nextInt();
 		
 		return choice;
 	}
@@ -20,20 +21,32 @@ public class Mainsol6 {
 		int choice, index, count = 0;
 		Student  key;
 		String isbn;
-
+        int rollno;
 		Scanner sc = new Scanner(System.in);
+		
 		Map<Integer, Student> map = new HashMap<>();
 		while ((choice = menu()) != 0) {
 			switch (choice) {
 			case 1:
-				Student  b = new Student();
-				b.accept();
-				map.put(b.getIsbn() ,b);
-				
-
+				Student s = new Student();
+				s.acceptStudent(); // implement method in Main class
+				map.put(s.getRollno(), s);
 				break;
-				// TODO Auto-generated method stub
-
+			case 2:
+				
+			 //  Scanner sc = new Scanner(System.in);//
+		        System.out.print("Enter rollno: ");
+		        int  Rollno = sc.nextInt();
+		        Student Student = map.get(Rollno);
+		       
+		}
+				break;
 	}
 
-}
+
+	
+	
+		
+	}}
+	
+	
