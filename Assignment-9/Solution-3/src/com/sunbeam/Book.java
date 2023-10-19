21df9d508168fc0bd2825e5d41c21b6fb02fa5dc
kd2-80179 <sonamchourasiya721@gmail.com>
2023-10-19 22:57:38 +0530

    assignment 8,9,10,11 uploaded

--- Assignment-9/Solution-3/src/com/sunbeam/Book.java
+++ Assignment-9/Solution-3/src/com/sunbeam/Book.java
@@ -137,6 +137,22 @@ public boolean equals(Object obj)
 		return false;
 	return true;
 }
+int choice;
+		System.out.println("------------------------------------------------------");
+		System.out.println("Menu");
+		System.out.println("1. Add new book in list");
+		System.out.println("2. Display all books in forward order");
+		System.out.println("3. Delete book at given index");
+		System.out.println("4. Check if book with given isbn is in list or not");
+		System.out.println("5. Delete all books in list");
+		System.out.println("6. Display number of books in list");
+		System.out.println("7. Sort all books by price in desc order");
+		System.out.print("Enter your choice = ");
+		choice = new Scanner(System.in).nextInt();
+		System.out.println("------------------------------------------------------");
+
+		return choice;
+	}
 
 	public static void main(String[] args) {
 		List<Book> list =new ArrayList<>();
--- Assignment-9/Solution-3/src/com/sunbeam/Demo02main.java
+++ Assignment-9/Solution-3/src/com/sunbeam/Demo02main.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Demo02main {
+static int menu() {
+		int choice;
+		System.out.println("------------------------------------------------------");
+		System.out.println("Menu");
+		System.out.println("1. Add new book in list");
+		System.out.println("2. Display all books in forward order");
+		System.out.println("3. Delete book at given index");
+		System.out.println("4. Check if book with given isbn is in list or not");
+		System.out.println("5. Delete all books in list");
+		System.out.println("6. Display number of books in list");
+		System.out.println("7. Sort all books by price in desc order");
+		System.out.print("Enter your choice = ");
+		choice = new Scanner(System.in).nextInt();
+		System.out.println("------------------------------------------------------");
+
+		return choice;
+	}
+
+
 
 	public static void main(String[] args) {
 		Student arr[] = new Student[5];
